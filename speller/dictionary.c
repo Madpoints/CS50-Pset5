3ec4bdebdc3e8c5ccd544b4583b135e18ce4ad53
John Furtado <jf8846@gmail.com>
2018-04-16 23:31:21 +0000

    speller complete

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -21,14 +21,16 @@ node;
 // trie root node
 node *root;
 
-// word counter
+// word counter for dictionary
 unsigned int numWords = 0;
 
-// returns a new initialized node pointer
+/*Helper functions for dictionary.c*/
+
+// Returns a new initialized node pointer
 struct node* makeNode(void)
 {
     // allocate space for and create a new node
-    struct node *newNode =(struct node*)malloc(sizeof(struct node));
+    struct node* newNode =(struct node*)malloc(sizeof(struct node));
 
     if (newNode != NULL)
     {
@@ -45,22 +47,27 @@ struct node* makeNode(void)
     return newNode;
 }
 
+
+// Deletes trie from the bottom up
 void deleteTrie(node* trie)
 {
+    // if no trie return
     if(!trie)
     {
         return;
     }
 
-    // recursive case (go to end of trie)
+    // using recursion go to last node of trie
     for (int i = 0; i < SIZE; i++)
     {
        deleteTrie(trie->children[i]);
     }
 
-    // base case
+    // remove node
     free(trie);
 }
+
+/*Main functions for dictionary.c*/
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
@@ -68,25 +75,34 @@ bool check(const char *word)
     // copy of root to iterate through trie
     node* trie = root;
 
+    // for each letter of word
     for (int i = 0; i < strlen(word); i++)
     {
+        // if apostrophe char
         if (word[i] == '\'')
         {
+            // check last index of children
+            // if false word not in dictionary
             if (!trie->children[SIZE - 1])
             {
                 return false;
             }
+            // else move to next node
             else
             {
                 trie = trie->children[SIZE - 1];
             }
         }
+        // if alpha char
         else
         {
+            // check chars index in children array
+            // if false word not in dictionary
             if (!trie->children[tolower(word[i]) - 'a'])
             {
                 return false;
             }
+            // else move to next node
             else
             {
                 trie = trie->children[tolower(word[i]) - 'a'];
@@ -94,11 +110,15 @@ bool check(const char *word)
         }
     }
 
+    // if at the end of word
+    // and is_word is false
+    // word not in dictionary
     if (!trie->is_word)
     {
         return false;
     }
 
+    // word found
     return true;
 }
 
@@ -133,44 +153,50 @@ bool load(const char *dictionary)
         // iterate through each letter
         while (word[index] != '\n')
         {
-            // if
+            // if apostrophe char
+            // insert node in the last index
             if (word[index] == '\'')
             {
+                // if no node exists make a new one
                 if (!trie->children[SIZE - 1])
                 {
                     trie->children[SIZE - 1] = makeNode();
                 }
 
+                // point to the current node in trie
                 trie = trie->children[SIZE - 1];
             }
+            // if alpha char
+            // insert node at chars alphabetic position in the children array
             else
             {
-                //printf("%c", c[index]);
+                // if no node exists make a new one
                 if (!trie->children[word[index] - 'a'])
                 {
                     trie->children[word[index] - 'a'] = makeNode();
                 }
 
+                 // point to the current node in trie
                 trie = trie->children[word[index] - 'a'];
             }
 
+            // next letter of word
             index++;
         }
 
+        // set is_word to true
         trie->is_word = true;
+        // increment number of words in dictionary
         numWords++;
     }
 
     fclose(file);
-
-    // TODO
     return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
     return numWords;
 }
 
